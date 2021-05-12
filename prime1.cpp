57f234f6a2513ae3d05b890c493aac76fc6628f8
srinu1085 <84057026+srinu1085@users.noreply.github.com>
2021-05-12 10:22:44 +0530

    Add files via upload

---
+++ prime1.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,count=0,i=1;
+    cin>>n;
+    while(i<=sqrt(n))
+    {
+        i++;
+        if(n%i==0)
+        {
+            cout<<"not a prime: "<<n;
+            count++;
+            break;
+        }
+    }
+    if(count==0)
+    {
+        cout<<"prime : "<<n;
+    }
+    
+    return 0;
+}
