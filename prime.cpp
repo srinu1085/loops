c8ea9524063857fc6b509cf9a02126837c30bb3a
srinu1085 <84057026+srinu1085@users.noreply.github.com>
2021-05-12 16:01:00 +0530

    Update prime.cpp

--- prime.cpp
+++ prime.cpp
@@ -22,12 +22,36 @@ class prime
         }
     }
 };
+class prime1
+{
+    public:
+    int count=0,i=2;
+    prime1(int n)
+    {
+        while(i<=n/2)
+        {
+            if(n%i==0)
+            { 
+                cout<<"not prime";
+                count++;
+                break;
+            }
+            i++;
+        }
+        if(count==0)
+        {
+            cout<<"prime";
+        }
+    }
+};
+
 
 int main()
 {
     int n;
     cin>>n;
     prime p=prime(n);
+    prime1 p1=prime(n);
  
 
     return 0;
