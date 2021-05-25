e783496bf6eca858f82296c7b681b4750265030b
srinu1085 <84057026+srinu1085@users.noreply.github.com>
2021-05-25 10:31:36 +0530

    Add files via upload

---
+++ handshake.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    long n,t,ans;
+
+    cin >>t;
+
+    while(t--)
+
+    {
+
+        cin>>n;
+
+        ans=n*(n-1)/2;
+
+        cout<<ans<<endl;
+
+ 
+
+    } 
+}                           
+
